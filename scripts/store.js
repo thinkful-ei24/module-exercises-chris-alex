4d88a0c8ad7a2608fae0d0cc6c04c7fb06de7c2c
Alex <0alexgutierrez@gmail.com>
2018-08-27 08:52:08 -0700

    finished project structure and module

---
+++ scripts/store.js
@@ -0,0 +1,21 @@
+// eslint-disable-next-line no-unused-vars
+const store = (function() {
+  //  const foo = 'bar';
+
+  const items = [
+    { id: cuid(), name: 'apples', checked: false },
+    { id: cuid(), name: 'oranges', checked: false },
+    { id: cuid(), name: 'milk', checked: true },
+    { id: cuid(), name: 'bread', checked: false }
+  ];
+  const hideCheckedItems = false;
+  const searchTerm = '';
+
+  return {
+    items,
+    hideCheckedItems,
+    searchTerm
+  };
+})();
+
+console.log('test');
